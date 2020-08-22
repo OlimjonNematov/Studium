0b1433974c783618c020c3a287d9c83e5fafcd07
Olimjon <olimjon.nematov@gmail.com>
2020-08-22 11:59:15 -0500

    fixed user auth bug

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
+import firebase from "firebase/app";
 import LogIn from "./components/Login";
 import SignUp from "./components/SignUp";
 import Title from "./components/Title";
 import { styles } from "./assets/rootStyle";
 function App() {
   //set up firebase------------------------------------------------------------------------------------
-  var firebaseConfig = {
-    apiKey: "AIzaSyDYlp-SHWM6nw63Zkum2A653jHLnlH6N3A",
-    authDomain: "studium-f4f92.firebaseapp.com",
-    databaseURL: "https://studium-f4f92.firebaseio.com",
-    projectId: "studium-f4f92",
-    storageBucket: "studium-f4f92.appspot.com",
-    messagingSenderId: "704796394157",
-    appId: "1:704796394157:web:6f68597ce6b6ad43ccfc24",
-    measurementId: "G-PK0Q1HHS4E",
-  };
+
   //---------------------------------------------------------------------------------------------------
   //decide wheather to show sign in or sign up
   const [showSignIn, setShowSignIn] = useState(true);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import firebase from "firebase/app";
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+var firebaseConfig = {
+  apiKey: "AIzaSyDYlp-SHWM6nw63Zkum2A653jHLnlH6N3A",
+  authDomain: "studium-f4f92.firebaseapp.com",
+  databaseURL: "https://studium-f4f92.firebaseio.com",
+  projectId: "studium-f4f92",
+  storageBucket: "studium-f4f92.appspot.com",
+  messagingSenderId: "704796394157",
+  appId: "1:704796394157:web:6f68597ce6b6ad43ccfc24",
+  measurementId: "G-PK0Q1HHS4E",
+};
+firebase.initializeApp(firebaseConfig);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
